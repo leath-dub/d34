c9467e1d195194c1ca90278cb19cef019a055393
leath-dub <77889270+leath-dub@users.noreply.github.com>
2022-12-21 16:29:17 +0000

    fixed typo

--- keymap.c
+++ keymap.c
@@ -25,7 +25,7 @@ process_record_user(uint16_t keycode, keyrecord_t *record)
 const uint16_t PROGMEM cmb_esc[] = {KC_D, KC_F, COMBO_END};
 const uint16_t PROGMEM cmb_bspc[] = {KC_J, KC_K, COMBO_END};
 combo_t key_combos[COMBO_COUNT] = {
-    COMBO(cmb_exc, KC_ESC),
+    COMBO(cmb_esc, KC_ESC),
     COMBO(cmb_bspc, KC_BSPC), // keycodes with modifiers are possible too!
 };
 
