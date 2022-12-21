80a1c497ab713ac973cd9d48eedf2513fd6f3b66
leath-dub <77889270+leath-dub@users.noreply.github.com>
2022-12-21 16:06:47 +0000

    fixed error about number of args to LAYOUT

--- keymap.c
+++ keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                                   XXXXXXX, LT_SPC,     MO(2),   XXXXXXX,
+                                   XXXXXXX, LT_SPC,     MO(2),   XXXXXXX
     ),
     [1] = LAYOUT(
         TG(3),   CTL_W,   XXXXXXX, XXXXXXX, XXXXXXX,    KC_QUOT, KC_HASH, XXXXXXX, KC_AT,   XXXXXXX,
