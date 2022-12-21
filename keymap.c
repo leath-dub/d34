9bea1865c34a28a9840d1f4e472e9d0c05b15de1
leath-dub <77889270+leath-dub@users.noreply.github.com>
2022-12-21 19:21:15 +0000

    test

--- keymap.c
+++ keymap.c
@@ -18,8 +18,8 @@ process_record_user(uint16_t keycode, keyrecord_t *record)
     switch (keycode) {
         case SFT_TG:
             if (record->event.pressed) {
-                register_code(OSM(KC_LALT));
-                register_code(OSL(5));
+                SEND_STRING(SS_TAP(OSM(KC_LALT)));
+                SEND_STRING(SS_TAP(OSL(5)));
             }
             return false;
             break;
